6429d8ed03f3f4fc10b245e36f6b72b7ea085ca4
Berkley Hill <rhill17@georgefox.edu>
2020-04-27 00:41:07 -0700

    fun with formatting w/ basic layout

--- src/PathfindingVisualizer/PathfindingVisualizer.jsx
+++ src/PathfindingVisualizer/PathfindingVisualizer.jsx
@@ -6,15 +6,47 @@ import './PathfindingVisualizer.css';
 export default class PathfindingVisualizer extends Component {
     constructor(parameters) {
         super(parameters);
-        this.state = {};
+        this.state = {
+            grid: [],
+        };
     }
 
+
+    componentDidMount() {
+        const grid = [];
+        for (let row = 0; row < 20; row++) {
+            const currentRow = [];
+            for (let column = 0; column < 60; column++) {
+                currentRow.push([]);
+            }
+            grid.push(currentRow);
+        }
+        this.setState({grid});
+    }
+
+
     render() {
+        const {grid} = this.state;
         return (
-            <div>
-                Foo
-                <Node></Node>
-            </div>
+            <>
+                <div className="header">
+                    <div className="inner_header">
+                        <div className="title">
+                            <h1> Pathfinding Visualizer</h1>
+                        </div>
+                        <div className="options">
+                            <button> Run </button>
+                        </div>
+                    </div>
+                </div>
+                <div className="grid">
+                    {grid.map((row, rowIndex) => {
+                        return <div>
+                            {row.map((node, nodeIndex) => <Node></Node>)}
+                        </div>
+                    })}
+                </div>
+            </>
         );
     }
 }
